e1cb23131a0edeb2cfac90b1d07c67e9ed8c2dc1
vaibhavnasit <vaibhavnasit@yahoo.com>
2019-04-01 02:41:35 +0530

    Implemented word count for spark scala pre-processor

--- src/main/scala/HelloScala.scala
+++ src/main/scala/HelloScala.scala
@@ -1,6 +1,27 @@
+import org.apache.spark.{SparkConf, SparkContext}
 object HelloScala {
 
-  def main(args: Array[String]): Unit = {
-    println("Hello Scala!!")
+  def main(args: Array[String]) {
+
+    //Create a SparkContext to initialize Spark
+    val conf = new SparkConf()
+    conf.setMaster("local")
+    conf.setAppName("Word Count")
+    val sc = new SparkContext(conf)
+
+    // Load the text into a Spark RDD, which is a distributed representation of each line of text
+    val textFile = sc.textFile("src/main/resources/shakespeare.txt")
+
+    //word count
+    val counts = textFile.flatMap(line => line.split(" "))
+      .map(word => (word, 1))
+      .reduceByKey(_ + _)
+
+    // Filter those words which are having more than 5000 occurrence
+    val my_count = counts.filter(_._2 >= 5000).sortBy(_._2)
+
+    my_count.foreach(println)
+    System.out.println("Total words: " + my_count.count());
+    //my_count.saveAsTextFile("/tmp/shakespeareWordCount")
   }
 }
